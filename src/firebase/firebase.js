3418a489ac8a734c8f8b2a721c9d331b4c7da19a
Dzseta <49132168+Dzseta@users.noreply.github.com>
2025-06-22 03:51:33 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,13 +3,7 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjQPGcoYcYKORfSuW-GCi746ow7A0RCNw",
-  authDomain: "ucc-events-c6e91.firebaseapp.com",
-  projectId: "ucc-events-c6e91",
-  storageBucket: "ucc-events-c6e91.firebasestorage.app",
-  messagingSenderId: "1033907615940",
-  appId: "1:1033907615940:web:84643214eec18e76673191",
-  measurementId: "G-3YTJB6F1V4"
+  // config
 };
 
 // Initialize Firebase
